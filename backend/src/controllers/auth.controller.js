a1042ea69035acaa75e57317a0e7c70cc25b18d0
unknown <alan8589824041@gmail.com>
2025-11-10 16:49:27 +0530

    sign up section refactoring

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,50 +4,48 @@ import bcrypt from 'bcrypt'
 
 export const SignUp = async (req, res) => {
     try {
-        const {fullName , email , password } = req.body
+        const { fullName, email, password } = req.body
 
-        if(!fullName || !email || !password ) {
-            return res.status(400).json({message: "All fileds are required"})
+        if (!fullName || !email || !password) {
+            return res.status(400).json({ message: "All fields are required" })
         }
 
         const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-        if(!emailRegex.test(email)){
-            return res.status(400).json({message: "Inavlid email format"})
+        if (!emailRegex.test(email)) {
+            return res.status(400).json({ message: "Invalid email format" })
         }
 
-        if(password.length < 6) {
-            return res.status(400).json({message: "Password should be atleast 6 characters"})
+        if (password.length < 6) {
+            return res.status(400).json({ message: "Password should be at least 6 characters" })
         }
 
-        const existingEmail = await User.findOne({email})
-        if(existingEmail){
-            return res.status(400).json({message: "Email already exist."})
+        const existingEmail = await User.findOne({ email })
+        if (existingEmail) {
+            return res.status(400).json({ message: "Email already exists" })
         }
 
         const salt = await bcrypt.genSalt(10)
         const hashedPassword = await bcrypt.hash(password, salt)
 
-        const newUser = User({
+        const newUser = new User({
             fullName,
             email,
             password: hashedPassword
         })
 
-        if(newUser){
-            generateToken(newUser._id, res)
-            await newUser.save()
+        const savedUser = await newUser.save()
 
-            res.status(201).json({
-                _id: newUser._id,
-                fullName: newUser.fullName,
-                email: newUser.email,
-                profilePic: newUser.profilePic
-            })
-        }else{
-            res.status(400).json({message: "invalid user data"})
-        }
+        generateToken(savedUser._id, res)
+
+        res.status(201).json({
+            _id: savedUser._id,
+            fullName: savedUser.fullName,
+            email: savedUser.email,
+            profilePic: savedUser.profilePic
+        })
+
     } catch (error) {
-        console.error('Error in SignUp controller', error.message);
-        res.status(500).json({message: "Internal server error"})
+        console.error('Error in SignUp controller:', error.message)
+        res.status(500).json({ message: "Internal server error" })
     }
 }
