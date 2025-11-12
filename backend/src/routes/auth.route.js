8d3bd4ad4bdd12f5b92f437cfad5f96b69969742
unknown <alan8589824041@gmail.com>
2025-11-12 12:24:30 +0530

    update profile added

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,15 @@
 import express from 'express'
-import { SignIn, SignOut, SignUp } from '../controllers/auth.controller.js'
+import { SignIn, SignOut, SignUp, updateProfile } from '../controllers/auth.controller.js'
+import { protectRoute } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
 router.post('/signup', SignUp)
 router.post('/signin', SignIn)
 router.post('/logout', SignOut)
+router.put('/update-profile', protectRoute, updateProfile)
+
+router.get('/check', protectRoute, (req, res) => {
+    res.status(200).json(req.user)
+})
 export default router
