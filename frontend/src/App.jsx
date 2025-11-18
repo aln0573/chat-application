d0469c466fdca5319b742590a3448f137180e860
unknown <alan8589824041@gmail.com>
2025-11-18 17:59:38 +0530

    route changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -27,7 +27,7 @@ import PageLoader from './components/PageLoader'
       <div className="absolute bottom-0 -right-4 size-96 bg-cyan-500 opacity-20 blur-[100px]" />
       <Routes>
       <Route path='/' element={authUser ? <ChatPage /> : <Navigate to={"/login"} />}/>
-      <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
+      <Route path='/signin' element={!authUser ? <LoginPage /> : <Navigate to={"/"} />} />
       <Route path='/signup' element={!authUser ? <SignUpPage /> : <Navigate to={"/"} />} />
      </Routes>
     </div>
