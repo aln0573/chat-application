9e140fbe65135e762525af71c71c23db743e6bf0
unknown <alan8589824041@gmail.com>
2025-11-18 15:16:58 +0530

    last changes

--- backend/src/server.js
+++ backend/src/server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT
 
 app.use(express.json({limit: "10mb"}))
 app.use(cors({
-    origin: "https://chat-application-frontend-4lv6.onrender.com",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 app.use(express.urlencoded({extended: true, limit: "10mb"}))
